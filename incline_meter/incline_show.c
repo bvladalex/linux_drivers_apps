352a0dc1b5ecafca39342a09edf9ed49238b731c
Vlad Barbalata <bvladalex6@gmail.com>
2021-06-28 13:45:01 -0700

    added some commnets for ease of use

--- incline_meter/incline_show.c
+++ incline_meter/incline_show.c
@@ -19,22 +19,28 @@
 #include <fcntl.h>
 #include <stdint.h>
 
+//paths for raw acceleration value files as created by the mpu6050 linux driver
 #define ACCEL_FILE_ACC_X "/sys/bus/iio/devices/iio:device0/in_accel_x_raw"
 #define ACCEL_FILE_ACC_Y "/sys/bus/iio/devices/iio:device0/in_accel_y_raw"
 #define ACCEL_FILE_ACC_Z "/sys/bus/iio/devices/iio:device0/in_accel_z_raw"
 
+//path to file created by the lcd linux driver
 #define LCD_FILE "/dev/lcd"
 
+//some constants
 #define ACC_FS_SENSITIVITY 16384
 #define FULL_SCALE_G_VALUE 2
 #define PI 				   3.1415926
 
+
 double x_val,y_val,z_val;
-int y_incline;
+int x_incline,y_incline,z_incline;
 char x_to_print[10], y_to_print[10], z_to_print[10];
 
+//declare some special char used to wipe/navigate display
 char special_chars[]="\f\n";
 char msg[20]="\fIncl in degrees:\n";
+//from datasheet of HD44780 controller, this is the hex value that when sent to display it'll display degree char
 int deg_char=0xDF;
 
 double get_acc_val(char *fd);
@@ -54,18 +60,17 @@ double get_acc_val(char *fd){
 		return 1;
 	}
 	int i=0;
+	//only way I got this to work was to read char by char until EOF
 	while(1) {
 		c = fgetc(proc);
 		if( feof(proc) ) {
 			break ;
 		}
-		//printf("%c", c);
 		if (c != '\0'){
 			Vo[i] = c;
 			++i;
 		}
 		Vo[i+1]= '\0';
-		//      Vo = c;
 	}
 	fclose(proc);
 	return (strtod(Vo,&ret_char_ptr)/ACC_FS_SENSITIVITY);
@@ -73,7 +78,7 @@ double get_acc_val(char *fd){
 
 int compute_incline(double axis1, double axis2){
 	int incl;
-
+	//derived from composition of forces formula
 	incl=(asin(axis1/sqrt(pow(axis1,2)+pow(axis2,2))))*180/PI;
 
 	return incl;
@@ -98,18 +103,17 @@ int main(){
 			if(my_dev<0){
 				perror("Fail to open device file: /dev/lcd");
 				}else{
-					//to modify below block to print message and incline angle
-
+					//convert from double to char to concatanate string that we'll display
 					gcvt(y_incline,2,y_to_print);
-					//snprintf((msg+18),3,"%s",y_to_print);
+					//concatanate mesage to print + current read value + special char (degree symbol)
 					snprintf((msg+18),4,"%s%c",y_to_print,deg_char_c);
+					//send a clear screen char
 					ret=write(my_dev,special_chars,1);
 					if(ret<0){
 						perror("Was not able send clear display command.\n");
 							}
-					//ret=write(my_dev,y_to_print,2);
+					//actual write of message
 					ret=write(my_dev,msg,21);
-					//ret=write(my_dev,&deg_char_c,1);
 					if(ret<0){
 						perror("Was not able to transfer all info.\n");
 							}
